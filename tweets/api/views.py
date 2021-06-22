9be1b8449b39c914f420ceb61a445e10ae179bc4
unknown <yunxiangchai@gmail.com>
2021-06-22 00:40:26 -0700

    redis cache

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -9,9 +9,7 @@ from utils.decorators import required_params
 from utils.paginations import EndlessPagination
 
 
-class TweetViewSet(viewsets.GenericViewSet,
-                   viewsets.mixins.CreateModelMixin,
-                   viewsets.mixins.ListModelMixin):
+class TweetViewSet(viewsets.GenericViewSet):
     """
     API endpoint that allows user to create, list tweets
     """
